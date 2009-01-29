aa620565e97bf39a0bd05a7ae321855e89df742f
william.killian <william.killian@90d0c292-ec0a-11dd-9c15-fdd50ba39c1e>
2009-01-29 00:04:29 +0000

    Updated algorithm in Test.java

--- Puzzles/src/Test.java
+++ Puzzles/src/Test.java
@@ -8,13 +8,16 @@ public class Test {
 		
 		ArrayList<String> words = Input.getFile();
 		
-		Iterator<String> iter = words.iterator();
-		while(iter.hasNext())
-		{
-			String word = iter.next();
-			System.out.println(word);
-		}
-		
+		// Iterator<String> iter = words.iterator();
+		// while(iter.hasNext())
+		// {
+		//  	String word = iter.next();
+		//  	System.out.println(word);
+		// }
+		for (String word : words) {		//Josh, Java has a nifty for-each loop that 
+			System.out.println (word);  //eliminates the need for the Iterator object.
+		}								//In fact, the for-each loop only works with
+										//things that implement Iterable
 		Input.saveWordsAs(words);
 	}
 }
