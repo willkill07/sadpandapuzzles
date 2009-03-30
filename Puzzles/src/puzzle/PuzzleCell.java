2fd2ed335095479c1000efbd83255fd6b8ba1c2d
william.killian <william.killian@90d0c292-ec0a-11dd-9c15-fdd50ba39c1e>
2009-03-30 17:49:30 +0000

    Crossword is working?

--- Puzzles/src/puzzle/PuzzleCell.java
+++ Puzzles/src/puzzle/PuzzleCell.java
@@ -12,113 +12,117 @@ import puzzle.Puzzle.Direction;
  * 
  */
 public class PuzzleCell {
-	/**
-	 * number of words associated with this PuzzleCell
-	 */
-	int numWords;
-	/**
-	 * the character associated with this PuzzleCell
-	 */
-	private char character;
-	
-	/**
-	 * the list of directions associated with this Puzzle Cell
-	 */
-	private ArrayList<Direction> dirs;
-	
-	/**
-	 * Default constructor. 
-	 */
-	public PuzzleCell () {
-		numWords = 0;
-		setCharacter('\0');
-		dirs = new ArrayList<Direction>();
-	}
-	
-	/**
-	 * Attempts to set the character value of this cell.
-	 * @param c - the next character to be added.
-	 * @return boolean - whether or not the add was successful
-	 */
-	public boolean add (char c) {
-		if(c == getCharacter() || getCharacter() == '\0'){
-		  numWords++;
-			setCharacter(c);
-			return true;
-		}
-		return false;
-		
-	}
-	
-	public boolean add (char c, Direction dir) {
-	  if(c == getCharacter() || getCharacter() == '\0'){
+  /**
+   * number of words associated with this PuzzleCell
+   */
+  int numWords;
+  /**
+   * the character associated with this PuzzleCell
+   */
+  private char character;
+  
+  /**
+   * the list of directions associated with this Puzzle Cell
+   */
+  private ArrayList<Direction> dirs;
+  
+  /**
+   * Default constructor. 
+   */
+  public PuzzleCell () {
+    numWords = 0;
+    setCharacter('\0');
+    dirs = new ArrayList<Direction>();
+  }
+  
+  /**
+   * Attempts to set the character value of this cell.
+   * @param c - the next character to be added.
+   * @return boolean - whether or not the add was successful
+   */
+  public boolean add (char c) {
+    if(c == getCharacter() || getCharacter() == '\0'){
+      numWords++;
+      setCharacter(c);
+      return true;
+    }
+    return false;
+    
+  }
+  
+  public boolean add (char c, Direction dir) {
+    if(c == getCharacter() || getCharacter() == '\0'){
       numWords++;
       setCharacter(c);
       dirs.add (dir);
       return true;
     }
     return false;
-	}
-	
-	/**
-	 * Adds a random character to the puzzleCell
-	 */
-	public void addRandomChar () {
-		setCharacter(Character.toChars(97 + (int)(Math.random()*26))[0]);
-	}
-	
-	/**
-	 * If a puzzle cell contains more then one word, this decrements the number of words
-	 * associated with this cell. If there is only one word, it decrements the number of words
-	 * then removes the character from the puzzle cell.
-	 */
-	public void remove () {
-		if (--numWords == 0) {
-			setCharacter('\0');
-		}
-	}
-	
-	public void remove (Direction dir) {
-	  dirs.remove (dir);
-	  if (--numWords == 0) {
-	    dirs.clear ();
+  }
+  
+  /**
+   * Adds a random character to the puzzleCell
+   */
+  public void addRandomChar () {
+    setCharacter(Character.toChars(97 + (int)(Math.random()*26))[0]);
+  }
+  
+  /**
+   * If a puzzle cell contains more then one word, this decrements the number of words
+   * associated with this cell. If there is only one word, it decrements the number of words
+   * then removes the character from the puzzle cell.
+   */
+  public void remove () {
+    if (--numWords == 0) {
       setCharacter('\0');
     }
-	}
-	
-	/**
-	 * Returns the contents of the puzzle cell as a string.
-	 * @return string - Returns the contents of the puzzle cell as a string.
-	 */
-	public String toString () {
-		return ((getCharacter() + "").toUpperCase());
-	}
+  }
+  
+  public void remove (Direction dir) {
+    dirs.remove (dir);
+    if (--numWords == 0) {
+      dirs.clear ();
+      setCharacter('\0');
+    }
+  }
+  
+  /**
+   * Returns the contents of the puzzle cell as a string.
+   * @return string - Returns the contents of the puzzle cell as a string.
+   */
+  public String toString () {
+    return ((getCharacter() + "").toUpperCase());
+  }
 
-	/**
-	 * Sets the char the puzzle cell contains.
-	 * @param character - Sets the char the puzzle cell contains.
-	 */
-	public void setCharacter(char character) {
-		this.character = character;
-	}
+  /**
+   * Sets the char the puzzle cell contains.
+   * @param character - Sets the char the puzzle cell contains.
+   */
+  public void setCharacter(char character) {
+    this.character = character;
+  }
 
-	/**
-	 * The character in the puzzle cell.
-	 * @return character - The character in the puzzle cell.
-	 */
-	public char getCharacter() {
-		return character;
-	}
-	
-	public int getNumWords() {
-	  return numWords;
-	}
-	
-	public void setNumWords(int i) {
-	  numWords = i;
-	}
-	
-	public boolean hasDirection (Direction dir) {
-	  return (dirs.contains (dir));
-	}
+  /**
+   * The character in the puzzle cell.
+   * @return character - The character in the puzzle cell.
+   */
+  public char getCharacter() {
+    return character;
+  }
+  
+  public int getNumWords() {
+    return numWords;
+  }
+  
+  public void setNumWords(int i) {
+    numWords = i;
+  }
+  
+  public boolean hasDirection (Direction dir) {
+    return (dirs.contains (dir));
+  }
+  
+  public boolean hasNoDirection () {
+    return (dirs.isEmpty ());
+  }
 }
