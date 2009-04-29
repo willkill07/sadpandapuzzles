b67f76ae298612e450ab4e5c8c3ae31b6cccd535
william.killian <william.killian@90d0c292-ec0a-11dd-9c15-fdd50ba39c1e>
2009-04-29 17:17:56 +0000

    Added Direction Enum as a separate source file

---
+++ Puzzles/src/puzzle/Direction.java
@@ -0,0 +1,26 @@
+package puzzle;
+
+/**
+ * All possible directions that words can be oriented
+ * 
+ * @author Sad Panda Software
+ * @version 3.0
+ */
+public enum Direction {
+  /** East */
+  EAST,
+  /** South */
+  SOUTH,
+  /** West */
+  WEST,
+  /** North */
+  NORTH,
+  /** Northeast */
+  NORTHEAST,
+  /** Southeast */
+  SOUTHEAST,
+  /** Southwest */
+  SOUTHWEST,
+  /** Northwest */
+  NORTHWEST
+};
